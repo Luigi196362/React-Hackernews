b8d60ae41533ddd4c145a466987615389630c712
Luis Enrique <wichobros@gmail.com>
2024-05-30 23:17:54 -0600

    first commit

---
+++ src/components/LinkList.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Link from './Link';
+
+import { useQuery, gql } from '@apollo/client';
+
+const FEED_QUERY = gql`
+  {
+      links {
+        id
+        url
+        description
+        votes {
+          id  
+        }
+        
+        postedBy {
+          username
+        } 
+
+      }
+ 
+  }
+`
+;
+
+const LinkList = () => {
+
+  const { data } = useQuery(FEED_QUERY);
+
+  return (
+    <div>
+    {data && (
+      <>
+        {data.links.map((link, index) => (
+          <Link key={link.id} link={link} index={index} />
+        ))}
+      </>
+     )}
+   </div>
+  );
+};
+
+export default LinkList;
